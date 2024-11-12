f5638f599676b1b0c46d34b62e6b9a1002480d56
xen9973 <115991927+xen9973@users.noreply.github.com>
2024-11-12 23:09:28 +0300

    Add files via upload

---
+++ ui/ui/MWM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ui.core;
+
+namespace ui.MWM.ViewModel
+{
+    internal class MainViewModel: ObservableObject
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
